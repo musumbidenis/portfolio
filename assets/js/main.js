ebb95b5809ab0cde52ce156e0b71ee59ea4687df
musumbidenis <musumbidenis@gmail.com>
2020-04-06 20:39:13 +0300

    Modified the portfolio items

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,20 +1,38 @@
-jQuery(window).on('load', function() {   
-    
-    // HIDE PRELAODER
-    $(".preloader").addClass("hide-preloader");
-  
+jQuery(window).on('load', function () {
+
+	// HIDE PRELAODER
+	$(".preloader").addClass("hide-preloader");
+
 });
 
 
-jQuery(document).ready(function($) {
+jQuery(document).ready(function ($) {
 	"use strict";
-    
-    // PORTFOLIO GALLERY 
-    $('.portfolio a').featherlightGallery({
-		previousIcon: '&#9664;',   
-		nextIcon: '&#9654;',         
+
+	// PORTFOLIO GALLERY 
+	$('.portfolio a').featherlightGallery({
+		previousIcon: '&#9664;',
+		nextIcon: '&#9654;',
 		galleryFadeIn: 100,
-		galleryFadeOut: 300    
+		galleryFadeOut: 300
 	});
-    
+
+});
+
+$('.mu-menu-btn').on('click', function(event) {
+	   
+	event.preventDefault();
+	
+	$('.mu-menu-full-overlay').addClass('mu-menu-full-overlay-show');
+   
+});
+
+// when click colose btn
+
+$('.mu-menu-close-btn').on('click', function(event) {
+	
+	event.preventDefault();
+	
+	$('.mu-menu-full-overlay').removeClass('mu-menu-full-overlay-show');
+	
 });
