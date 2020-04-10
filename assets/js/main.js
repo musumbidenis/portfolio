90bc732920415c896b811ec6ab98ba4c88c524c6
musumbidenis <musumbidenis@gmail.com>
2020-04-10 15:25:14 +0300

    Modified the portfolio

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,35 +4,3 @@ jQuery(window).on('load', function () {
 	$(".preloader").addClass("hide-preloader");
 
 });
-
-
-jQuery(document).ready(function ($) {
-	"use strict";
-
-	// PORTFOLIO GALLERY 
-	$('.portfolio a').featherlightGallery({
-		previousIcon: '&#9664;',
-		nextIcon: '&#9654;',
-		galleryFadeIn: 100,
-		galleryFadeOut: 300
-	});
-
-});
-
-$('.mu-menu-btn').on('click', function(event) {
-	   
-	event.preventDefault();
-	
-	$('.mu-menu-full-overlay').addClass('mu-menu-full-overlay-show');
-   
-});
-
-// when click colose btn
-
-$('.mu-menu-close-btn').on('click', function(event) {
-	
-	event.preventDefault();
-	
-	$('.mu-menu-full-overlay').removeClass('mu-menu-full-overlay-show');
-	
-});
